31b21808a4088874581737ca70b320e224170d18
unknown <c1007348@newcastle.ac.uk>
2023-05-06 18:47:50 +0100

    Added base html file

--- Users/forms.py
+++ Users/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
-class LoginForm():
+
+class LoginForm(FlaskForm):
     username = StringField(render_kw={"size": 50, "maxlength": 70})
     password = PasswordField(render_kw={"size": 50, "maxlength": 70})
     submit = SubmitField(render_kw={"size": 50, "maxlength": 70})
 
 
-class RegisterForm():
+class RegisterForm(FlaskForm):
     username = StringField(render_kw={"size": 50, "maxlength": 70})
     password = PasswordField(render_kw={"size": 50, "maxlength": 70})
     confirm_password = PasswordField(render_kw={"size": 50, "maxlength": 70})
