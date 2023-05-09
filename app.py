e08de0e150785171315e6ebee6e896495fae1ebb
Alexander MacMillan <91530486+alex-macmillan@users.noreply.github.com>
2023-05-09 10:56:39 +0100

    Alex's branch (#1)
    * - initialised database in app.py
    
    - created database tables: users, orders and inventory
    
    * - edited database connection details in app.py
    - changed database table setup to include OrderItems table to store items for the order
    
    ---------
    
    Co-authored-by: alex-macmillan <alexmacmillan@protonmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
-
+# initialise database
+engine = create_engine('mariadb:///csc2033_team37:BikeRode4out@cs-db.ncl.ac.uk:3306/csc2033_team37')
+# engine = create_engine("jdbc:mariadb://cs-db.ncl.ac.uk:3306/csc2033_team37")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://csc2033_team37:BikeRode4out@cs-db.ncl.ac.uk/csc2033_team37'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
