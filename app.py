5bc29e3acb9cfed76ede7446a6af9aad1824f8cb
Alexander MacMillan <91530486+alex-macmillan@users.noreply.github.com>
2023-05-17 13:37:53 +0100

    integrated index() function to load index page upon startup and re-organised app.py to prevent functions starting in wrong order

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,34 +14,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from flask import redirect
-# BLUEPRINTS
-# import blueprints
-from user.views import users_blueprint
-from producer.view import producer_blueprint
-from consumer.view import consumer_blueprint
 
 # imports LoginManageer
 from flask_login import LoginManager, current_user
 from models import User
 
-# # register blueprints with app
-app.register_blueprint(users_blueprint)
-app.register_blueprint(producer_blueprint)
-app.register_blueprint(consumer_blueprint)
+@app.route('/')
+def index():
+    return render_template('main/index.html')
 
 # define login manager
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.init_app(app)
 
+# BLUEPRINTS
+# import blueprints
+from user.views import users_blueprint
+from producer.view import producer_blueprint
+from consumer.view import consumer_blueprint
+
+# # register blueprints with app
+app.register_blueprint(users_blueprint)
+app.register_blueprint(producer_blueprint)
+app.register_blueprint(consumer_blueprint)
+
+
 @login_manager.user_loader
 def load_user(email):
     return User.query.get(int(email))
-
-@app.route('/')
-def index():
-    redirect_url = url_for('dashboard')
-    return redirect(redirect_url)
 
 @app.route('/dashboard')
 def dashboard():
