156996873f0798bdb9a5cf4bef91af6e846da299
Sreejith Kalathil <84213736+sree-99-s@users.noreply.github.com>
2023-05-22 15:04:14 +0100

    Updated user/views.py

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, session, Markup, request
 from models import User, Orders
 from app import db
-from user.forms import RegisterForm, LoginForm
+from user.forms import  LoginForm
 import bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
@@ -11,39 +11,7 @@ from flask_mail import Message, Mail
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
-@users_blueprint.route('/register', methods=['GET', 'POST'])
-def register():
-    # create signup form object
-    form = RegisterForm()
 
-    # if request method is POST or form is valid
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        # if this returns a user, then the email already exists in database
-
-        # if email already exists redirect user back to signup page with error message so user can try again
-        if user:
-            flash('Email address already exists')
-            return render_template('users/register.html', form=form)
-
-        # create a new user with the form data
-        new_user = User(email=form.email.data,
-                        firstname=form.firstname.data,
-                        lastname=form.lastname.data,
-                        phone=form.phone.data,
-                        password=form.password.data,
-                        postcode=form.postcode.data,
-                        role=form.role.data)
-
-        # add the new user to the database
-        db.session.add(new_user)
-        db.session.commit()
-
-        # sends user to login page
-        logging.warning('SECURITY - User registration [%s, %s]', form.email.data, request.remote_addr)
-        return redirect(url_for('users/login.html'))
-    # if request method is GET or form not valid re-render signup page
-    return render_template('users/register.html', form=form)
 
 
 @users_blueprint.route('/login', methods=['GET', 'POST'])
@@ -114,6 +82,7 @@ def account():
 @users_blueprint.route('/logout')
 @login_required
 def logout():
+    # Data is recorded in lottery.log each time a user logs out of the program
     logging.warning('SECURITY - Log out [%s, %s, %s]', current_user.id, current_user.email, request.remote_addr)
     #Function for the user to log out
     logout_user()
@@ -161,3 +130,5 @@ def get_consumer_mail(order_id):
         consumer = User.query.get(order.consumer_id)
         return [consumer.email]
     return []
+
+
