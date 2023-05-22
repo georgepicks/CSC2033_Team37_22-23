67d8e0685d821c617dc4842d7da0a444da4ef9df
Sreejith Kalathil <84213736+sree-99-s@users.noreply.github.com>
2023-05-22 13:21:05 +0100

    Updated apps.py with render templates for non functioning pages

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 pymysql.install_as_MySQLdb()
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -15,46 +16,71 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://csc2033_team37:BikeRode4out@c
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 from flask import redirect
-# BLUEPRINTS
-# import blueprints
-from user.views import users_blueprint
-from producer.view import producer_blueprint
-from consumer.view import consumer_blueprint
+
 
 # imports LoginManageer
 from flask_login import LoginManager, current_user
 from models import User
 
-# # register blueprints with app
-app.register_blueprint(users_blueprint)
-app.register_blueprint(producer_blueprint)
-app.register_blueprint(consumer_blueprint)
+@app.route('/')
+def index():
+    return render_template('main/index.html')
+
 
 # define login manager
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.init_app(app)
 
+# BLUEPRINTS
+# import blueprints
+from user.views import users_blueprint
+from producer.view import producer_blueprint
+from consumer.view import consumer_blueprint
+
+
+# # register blueprints with app
+app.register_blueprint(users_blueprint)
+app.register_blueprint(producer_blueprint)
+app.register_blueprint(consumer_blueprint)
+
+
 @login_manager.user_loader
 def load_user(email):
     return User.query.get(int(email))
 
-@app.route('/')
-def index():
-    redirect_url = url_for('dashboard')
-    return redirect(redirect_url)
 
 @app.route('/dashboard')
 def dashboard():
     return 'Welcome to the dashboard!'
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/')
+def about_us():
+    return render_template('other_pages/about_us.html')
+
+
+@app.route('/')
+def contact():
+    return render_template('other_pages/contact.html')
+
+
+@app.route('/')
+def privacy():
+    return render_template('other_pages/privacy.html')
+
+
+@app.route('/')
+def terms():
+    return render_template('other_pages/terms.html')
+
+
 if __name__ == '__main__':
     app.run()
-
-
