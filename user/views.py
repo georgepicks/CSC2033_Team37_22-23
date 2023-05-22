6d2f5f7fbb543bdbce897bada6e410e174e5fdca
Sreejith Kalathil <84213736+sree-99-s@users.noreply.github.com>
2023-05-22 14:02:10 +0100

    Update views.py

--- user/views.py
+++ user/views.py
@@ -114,7 +114,6 @@ def account():
 @users_blueprint.route('/logout')
 @login_required
 def logout():
-    # Data is recorded in lottery.log each time a user logs out of the program
     logging.warning('SECURITY - Log out [%s, %s, %s]', current_user.id, current_user.email, request.remote_addr)
     #Function for the user to log out
     logout_user()
