5c9bf48ce3db60a02a7e0d2e7e8ebfba8d5e1d37
Sreejith Kalathil <84213736+sree-99-s@users.noreply.github.com>
2023-05-23 18:40:13 +0100

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, RadioField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, Regexp
 import re
 
@@ -12,7 +12,6 @@ def character_check(form,field):
         if char in excluded_chars:
             raise ValidationError(f"Character {char} is not allowed.")
 
-            
 # define password validator functions
 def validate_password(self, data_field):
     p = re.compile(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])')
@@ -20,14 +19,12 @@ def validate_password(self, data_field):
         raise ValidationError(
             "Password should contain 1 uppercase letter, lowercase letters, one digit and should be atleast 6 charscters with one special character")
 
-        
 # Define phone validate function
 def validate_phone(self,data_field):
-    i = re.compile(r'(?=\d\d\d\d-\d\d\d-\d\d\d\d)')
+    i = re.compile(r'(?=\d\d\d\d\d\d\d\d\d\d\d)')
     if not i.match(data_field.data):
         raise ValidationError("Phone number must be of the format XXXX-XXX-XXX")
 
-# Define postcode validate function       
 def validate_postcode(self, data_field):
     c = re.compile(r'^[A-Za-z]{1,2}[0-9][0-9A-Za-z]?\s?[0-9][A-Za-z]{2}$')
     if not c.match(data_field.data):
@@ -48,14 +45,12 @@ class ConsumerRegisterForm(FlaskForm):
         DataRequired(), validate_postcode],render_kw={"size": 50, "maxlength": 70})
     submit = SubmitField(validators=[DataRequired()],render_kw={"size": 50, "maxlength": 50})
 
-    
 class LoginForm(FlaskForm):
     email = StringField(validators=[DataRequired(), Email()],render_kw={"size": 50, "maxlength": 70})
     password = PasswordField('password',
                              validators=[DataRequired()],render_kw={"size": 50, "maxlength": 70})
     submit = SubmitField(validators=[DataRequired()],render_kw={"size": 50, "maxlength": 50})
 
-    
 class ProducerRegisterForm(FlaskForm):
     email = StringField(validators=[DataRequired(), Email()], render_kw={"size": 50, "maxlength": 70})
     producer_name = StringField(validators=[DataRequired(), character_check],render_kw={"size": 50, "maxlength": 70})
