0f310f7595a29a0c49814f3b91e03f725abca29a
SamRobertson25 <s.robertson5@newcastle.ac.uk>
2023-05-26 17:58:22 +0100

    changed link for user account in producer view.py

--- consumer/view.py
+++ consumer/view.py
@@ -214,11 +214,11 @@ def find_producers(distance_range):
 
 
 # view user account
-@consumer_blueprint.route('/account')
+@consumer_blueprint.route('/consumer_account')
 @login_required
-def account():
-    # Shows the account details of the user
-    return render_template('users/account.html',
+def consumer_account():
+    # Shows the account details of the consumer
+    return render_template('users/consumer_acc.html',
                            id=current_user.id,
                            email=current_user.email,
                            firstname=current_user.firstname,
