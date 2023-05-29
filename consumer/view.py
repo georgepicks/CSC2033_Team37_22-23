60299f25d2e99ab0d980a45868918b0acfa7aa0d
Sreejith Kalathil <84213736+sree-99-s@users.noreply.github.com>
2023-05-29 17:06:00 +0100

    Update view.py

--- consumer/view.py
+++ consumer/view.py
@@ -1,28 +1,16 @@
-"""
-File: consumer/view.py
-Authors: Sreejith Sudhir Kalathil, George Pickard, Alexander MacMillan
-Description: Provides all the functionality specific to consumer users. This includes their specific register form, as
-well as displaying the feed, allowing them to filter or search for producers in the feed, and CREATE, UPDATE and
-DELETING orders.
-"""
 import pgeocode
 from flask import Blueprint, render_template, request, session, redirect, url_for, jsonify, flash
 from flask_login import login_required, current_user
 from models import Consumer, InventoryItems, OrderItems, Orders, Producer
 from app import app, db
 from datetime import datetime
-from user import views
 from user.forms import ConsumerRegisterForm
 
 consumer_blueprint = Blueprint('consumer', __name__, template_folder='templates')
 
 
-@consumer_blueprint.route('/consumer/register', methods=['GET', 'POST'])
+@consumer_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
-    """
-    Called when a user is redirected to consumer/register, calls upon the ConsumerRegisterForm() in forms.py, when user
-    submits it creates a new consumer in the consumer table then redirects them to log into their new account.
-    """
     # create signup form object
     form = ConsumerRegisterForm()
 
@@ -57,10 +45,6 @@ def register():
 @consumer_blueprint.route('/feed', methods=['GET', 'POST'])
 @login_required
 def feed():
-    """
-    Called when user redirects to /feed, collects all producers and displays them as buttons which consumer can click on to
-    proceed to /order
-    """
     suppliers = []  # Create an empty list to store supplier information
 
     # Retrieve all producers from the database
@@ -84,11 +68,8 @@ def feed():
 @app.route('/order', methods=['GET', 'POST'])
 @login_required
 def order_generate():
-    """
-    Called when the consumer selects a producer, displays information about the producer and all of the items they have
-    available in their inventory
-    """
     supplier_id = request.args.get('supplier_id')
+
     supplier = Producer.query.filter_by(id=supplier_id).first()
 
     name = supplier.producer_name
@@ -109,13 +90,79 @@ def order_generate():
             'producer': item.producer,
             'dietary': item.dietary
         }
+
         items.append(item_data)
 
+        print(items)
 
     return render_template('consumer/order.html', items=items, supplier_name=name, supplier_address1=address1,
-                           supplier_address2=address2, supplier_address3=address3, supplier_postcode=postcode)
+                           supplier_address2=address2,supplier_address3=address3, supplier_postcode=postcode)
 
 
+# Function to search for an item in the inventory
+@app.route('/feed', methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        query = request.form.get('query')  # Retrieve the search query from the form
+
+        # Searching using SQLALCHEMY
+        results = InventoryItems.item.query.filter(InventoryItems.item.name.ilike(f'%{query}%')).all()
+
+        if not results:
+            message = "No items found matching your search query."
+
+        return render_template('consumer/feed.html', results=results, message=message)
+
+    return render_template('consumer/feed.html')
+
+
+# Function that allows to show items by a dietary filter
+@app.route('/food/<food_type>')
+@login_required
+def filter_by_dietary(food_type):
+    # Retrieve the items based on the food type
+    items = InventoryItems.query.filter(InventoryItems.dietary.ilike(food_type)).all()
+
+    if not items:
+        # Returns error if no item is found
+        return jsonify({
+            'success': False,
+            'error': 'No items found for the specified food type.'
+        }), 404
+
+    results = [item.format() for item in items]
+
+    return jsonify({
+        'success': True,
+        'results': results,
+        'count': len(results)
+    })
+
+
+# The Function returning a list of all the relevant items from the search
+@app.route('/search_results/<query>')
+@login_required
+def search_results(query):
+    results = Consumer.query.whoosh_search(query).all()
+    return render_template('search_results.html', query=query, results=results)
+
+
+# Function that allows the user to edit or make changes to the order before confirmation
+@app.route('/order/edit/<int:order_id>', methods=['GET', 'POST'])
+@login_required
+def edit_order(order_id):
+    order = OrderItems.query.get_or_404(order_id)
+    if request.method == 'POST':
+        order.item = request.form['item']
+        order.quantity = request.form['quantity']
+        db.session.commit()
+        return redirect(url_for(''))
+    else:
+        return render_template('', order=order)
+
+
+# Function to place an order
 # Function to place an order
 @app.route('/place_order', methods=['GET', 'POST'])
 @login_required
@@ -152,72 +199,8 @@ def place_order():
     return render_template("consumer/order_confirm.html", order_id=order_id)
 
 
-
-@app.route('/feed', methods=['GET', 'POST'])
-@login_required
-def search():
-    if request.method == 'POST':
-        query = request.form.get('query')  # Retrieve the search query from the form
-
-        # Searching using SQLALCHEMY
-        results = InventoryItems.item.query.filter(InventoryItems.item.name.ilike(f'%{query}%')).all()
-
-        if not results:
-            message = "No items found matching your search query."
-
-        return render_template('search_results.html', results=results, message=message)
-
-    return render_template('search.html')
-
-
-# Function that allows to show items by a dietary filter
-@app.route('/food/<food_type>')
-@login_required
-def filter_by_dietary(food_type):
-    # Retrieve the items based on the food type
-    items = InventoryItems.query.filter(InventoryItems.dietary.ilike(food_type)).all()
-
-    if not items:
-        # Returns error if no item is found
-        return jsonify({
-            'success': False,
-            'error': 'No items found for the specified food type.'
-        }), 404
-
-    results = [item.format() for item in items]
-
-    return jsonify({
-        'success': True,
-        'results': results,
-        'count': len(results)
-    })
-
-
-# The Function returning a list of all the relevant items from the search
-@app.route('/search_results/<query>')
-@login_required
-def search_results(query):
-    results = Consumer.query.whoosh_search(query).all()
-    return render_template('search_results.html', query=query, results=results)
-
-
-@app.route('/order/edit/<int:order_id>', methods=['GET', 'POST'])
-@login_required
-def edit_order(order_id):
-    """
-    Allows the user to edit or make changes to the order before confirmation
-    """
-    order = OrderItems.query.get_or_404(order_id)
-    if request.method == 'POST':
-        order.item = request.form['item']
-        order.quantity = request.form['quantity']
-        db.session.commit()
-        return redirect(url_for(''))
-    else:
-        return render_template('', order=order)
-
-
-# Shows the information about the order
+# Function to cancel an order made within a timeframe
+# Function to place an order
 @app.route('/place_order-order', methods=['GET', 'POST'])
 @login_required
 def order_details():
@@ -237,47 +220,13 @@ def order_details():
         }
         order_list.append(order_dict)
 
-
     db.session.commit()
-
 
     return render_template('consumer/consumer_orders.html', orders_list=order_list)
 
 
-@app.route('/cancel-order', methods=['POST'])
-@login_required
-def cancel_order(order_id):
-    """
-    If the cancellation deadline has not expired, enables the consumer to delete an Order object
-    """
-    # Retrieve the order with the given order ID from the database
-    order = Orders.query.get(order_id)
-
-    if order:
-        cancellation_deadline = session.get('cancellation_deadline')
-        if cancellation_deadline and datetime.now() < cancellation_deadline:
-            # Perform cancellation logic
-            db.session.delete(order)  # Delete the order from the database
-            db.session.commit()
-            session.pop('selected_products', None)
-            session.pop('cancellation_deadline', None)
-            flash('Order is cancelled')
-            views.cancel_mail(order_id)
-        else:
-            flash('Cancellation period has expired.')
-    else:
-        flash('Order not found.')
-
-    return redirect(url_for('order'))
-
-
 @app.route('/')
 def find_producers(distance_range):
-    """
-    Called before loading the feed, by default just displays them all to the consumer, but if they add a maximum distance
-    filter, compares the producer's postcodes against that of the user, removes from the display the ones that are too far
-    away, and sorts the rest by distance from the consumer.
-    """
     # if user has not yet specified a distance
     if distance_range == 0:
         producers = Producer.query.all()
@@ -287,6 +236,7 @@ def find_producers(distance_range):
         nearby_producers = {}
         # query all producers
         producers = Producer.query.all()
+        # ------------ need to add distance functionality in later ----------------
         for i in producers:
             # calculate distance between all producers and current user
             distance = geodistance.query_postal_code(current_user.postcode, i.postcode)
@@ -301,9 +251,6 @@ def find_producers(distance_range):
 @consumer_blueprint.route('/consumer_account/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_consumer_account(id):
-    """
-    Enables the consumer to edit their personal details
-    """
     consumer = Consumer.query.get_or_404(id)
     if request.method == 'POST':
         consumer.email = request.form['email']
@@ -315,4 +262,5 @@ def edit_consumer_account(id):
         return redirect(url_for('users.account'))
     else:
         return render_template('users/edit_account.html', consumer=consumer)
+
 
